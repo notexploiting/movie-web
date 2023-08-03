6fb46354b6f04d0ecc5cdf3b99df10521a0cd2ff
Chris <58007532+notexploiting@users.noreply.github.com>
2023-08-03 17:43:00 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+window.__CONFIG__ = {
+  // url must NOT end with a slash
+  VITE_CORS_PROXY_URL: "https://movie-web.tinyciri.workers.dev",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjYWIyYWMyNmFlNGNlNzFlYTUyMDQzMDAyMjc2M2VmMCIsInN1YiI6IjY0Y2MxZTNiMjk3MzM4MDIwZTU3YWY3NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.d7TnvZINQ4sgB2EQTaMUHBKRvywpXGKGESawV7Ymelw"
+};
